1a01c7d457011c6580073c5b75d26833f8f487de
Prince <princekr081001@gmail.com>
2023-05-16 15:04:46 +0530

    first commit

---
+++ Propuse.cpp
@@ -0,0 +1,178 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+
+
+int main()
+{
+	string a;
+	HANDLE h=GetStdHandle(STD_OUTPUT_HANDLE);
+	cout<<"\n\n\t\t Enter your love name:"; 
+	cin>>a;
+	system("cls");
+	cout<<"\n\n\t\t My Love "<<"This is special for you\n\n";
+	
+	cout<<endl;
+	cout<<endl;
+	cout<<"   ***      ***        ***         *****    *****      **********        **********               ********              **********          **********    "<<endl;
+	cout<<"   ***     *****      *****        *****    *****      ***********      ***********             *************           ***********        ***********    "<<endl;
+	cout<<"   ***    *******    *******       *****    *****      *****  *****    *****  *****           ******     ******         *****  *****      *****  *****    "<<endl;
+	cout<<"   ***   *********  *********      *****    *****      *****    ***** *****   *****          *****         *****        *****    *****  ******   *****    "<<endl;
+	cout<<"   ***   *********  *********      *****    *****      *****      ********    *****         *****           *****       *****      *********     *****    "<<endl;
+	cout<<"   ***   ********************      *****    *****      *****        ****      *****        *****             *****      *****         ****       *****    "<<endl;
+	cout<<"   ***    ******************       *****    *****      *****                  *****       *****               *****     *****                    *****    "<<endl;
+	cout<<"   ***     ****************        *****    *****      *****                  *****       *****                *****    *****                    *****    "<<endl;
+	cout<<"   ***      **************         *****    *****      *****                  *****       *****                *****    *****                    *****    "<<endl;
+	cout<<"   ***       ************          *****    *****      *****                  *****       *****                *****    *****                    *****    "<<endl;
+	cout<<"   ***        **********           *****    *****      *****                  *****        *****              *****     *****                    *****    "<<endl;
+	cout<<"   ***         ********            *****    *****      *****                  *****         *****             *****     *****                    *****    "<<endl;
+	cout<<"   ***          ******             *****    *****      *****                  *****          *****           *****      *****                    *****    "<<endl;
+	cout<<"   ***           ****              **************      *****                  *****             *****      *****        *****                    *****    "<<endl;
+	cout<<"   ***            **                ************       *****                  *****              *************          *****                    *****    "<<endl;
+	cout<<"   ***             *                 **********        *****                  *****                *********            *****                    *****    "<<endl;
+	cout<<endl;
+	
+	SetConsoleTextAttribute(h,1);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,2);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,3);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,4);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,5);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,6);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,7);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,8);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,9);
+	cout<<"\t"<<a;
+	
+	
+	SetConsoleTextAttribute(h,10);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,11);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,12);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,13);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,14);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,15);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,16);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,17);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,18);
+	cout<<"\t"<<a;
+	
+	SetConsoleTextAttribute(h,1);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,2);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,3);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,4);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,5);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,6);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,7);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,8);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,9);
+	cout<<"\t"<<a;
+	
+	SetConsoleTextAttribute(h,1);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,2);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,3);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,4);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,5);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,6);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,7);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,8);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,9);
+	cout<<"\t"<<a;
+	
+	SetConsoleTextAttribute(h,1);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,2);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,3);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,4);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,5);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,6);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,7);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,8);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,9);
+	cout<<"\t"<<a;
+	
+	SetConsoleTextAttribute(h,1);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,2);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,3);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,4);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,5);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,6);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,7);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,8);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,9);
+	cout<<"\t"<<a;
+	
+	SetConsoleTextAttribute(h,1);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,2);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,3);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,4);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,5);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,6);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,7);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,8);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,9);
+	cout<<"\t"<<a;
+	
+	SetConsoleTextAttribute(h,1);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,2);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,3);
+	cout<<"\t"<<a;
+	SetConsoleTextAttribute(h,4);
+	cout<<"\t"<<a;
+}
---
+++ program1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+  int a /*,b,sum,substraction,multiplication*/,square;
+ /*	cout<<"enter two number";
+ 	cin>>a>>b>>square;
+ 	sum=a+b;
+ 	
+ 	cout<<sum<<endl;
+ 	
+ 	substraction=a-b;
+ 	cout<<substraction<<endl;
+ 	
+ 	multiplication=a*b;
+ 	cout<<multiplication<<endl;*/
+ 	
+ 	cout<<"enter the number";
+ 	
+ 	square=a ;
+ 	
+ 	cout<<square<<endl;
+ 	
+ 	
+ 	return 0;
+ 	
+ 	
+ }
